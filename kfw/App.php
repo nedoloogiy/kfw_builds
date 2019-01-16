f5efade7bad5e128207958ac9b569f3bb9b995fd
Nedoloogiy <Nedoloogiy@mail.ru>
2019-01-16 03:58:24 +0300

    bug fix

---
+++ kfw/App.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kosoy
+ * Date: 16.01.2019
+ * Time: 3:53
+ */
+
+namespace kfw;
+
+
+class App
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
